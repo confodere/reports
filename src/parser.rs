731ac7660cf19ebd84c86aa19890946eef3aed18
confodere <l.tuppercreed@gmail.com>
2022-02-04 23:32:43 +1100

    added positional tracking to parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,6 +1,7 @@
 use nom::{
     branch::alt,
     bytes::complete::{tag, take, take_until},
+    character::complete::multispace0,
     error::Error,
     error::{ErrorKind, ParseError},
     sequence::{preceded, terminated, tuple},
@@ -10,7 +11,11 @@ use nom::{
 /// Opening tag for a block element
 fn opening(i: &str) -> IResult<&str, (&str, &str, &str)> {
     tuple((
-        preceded(tag("{{"), alt((tag("Change"), tag("AvgFreq")))),
+        preceded(
+            tag("{{"),
+            // terminated removes the space after the keyword
+            terminated(alt((tag("Change"), tag("AvgFreq"))), multispace0),
+        ),
         take_until("}}"),
         tag("}}"),
     ))(i)
@@ -59,14 +64,50 @@ pub fn either(i: &str) -> IResult<&str, &str> {
     }
 }
 
-pub fn find_blocks(i: &str) -> IResult<&str, Vec<(&str, &str, &str)>> {
+pub fn find_blocks(i: &str) -> IResult<&str, Vec<Block>> {
     // Initial either is to remove any preceding junk
-    let (i, _) = either(i)?;
-    alternate(either, tag_block)(i)
+    let (i, initial_junk) = either(i)?;
+    let (residual, block_tups) = alternate(either, tag_block)(i)?;
+
+    let padding = initial_junk.len();
+    let mut blocks: Vec<Block> = Vec::new();
+    for ((keyword, vars, template), start, end) in block_tups {
+        blocks.push(Block {
+            keyword,
+            vars,
+            template,
+            start: start + padding,
+            end: end + padding,
+        });
+    }
+
+    Ok((residual, blocks))
 }
 
-/// Alternate between two parsers to produce a list of elements
+#[derive(Debug, PartialEq, Eq)]
+pub struct Block<'a> {
+    pub keyword: &'a str,
+    pub vars: &'a str,
+    pub template: &'a str,
+    pub start: usize,
+    pub end: usize,
+}
+
+impl Default for Block<'_> {
+    fn default() -> Self {
+        Self {
+            keyword: "Change",
+            vars: "Weekly",
+            template: "Weekly change was {{fig}}.",
+            start: 0,
+            end: 54,
+        }
+    }
+}
+
+/// Alternate between two parsers to produce a list of elements matching the second parser
 /// , but allows a zero width separator
+/// as well as returning the start and end positions of the element
 ///
 /// An adjusted form of nom's `separated_list0`
 ///
@@ -79,14 +120,17 @@ pub fn find_blocks(i: &str) -> IResult<&str, Vec<(&str, &str, &str)>> {
 /// use nom::{Err, error::ErrorKind, Needed, IResult};
 /// use nom::bytes::complete::tag;
 ///
-/// fn parser(s: &str) -> IResult<&str, Vec<&str>> {
+/// fn parser(s: &str) -> IResult<&str, Vec<(&str, usize, usize)>> {
 ///     alternate(either, tag("{{Change}}"))(s)
 /// }
 ///
-/// assert_eq!(parser("{{Change}}junk{{Change}}more junk{{Change}}"), Ok(("", vec!["{{Change}}", "{{Change}}", "{{Change}}"])));
-/// assert_eq!(parser("{{Change}}junk{{Change}}more junk{{Change}}{{Change}}"), Ok(("", vec!["{{Change}}", "{{Change}}", "{{Change}}", "{{Change}}"])));
+/// assert_eq!(parser("{{Change}}junk{{Change}}more junk{{Change}}"), Ok(("", vec![("{{Change}}",0, 10), ("{{Change}}", 14, 24), ("{{Change}}", 33, 43)])));
+/// assert_eq!(parser("{{Change}}junk{{Change}}more junk{{Change}}{{Change}}"), Ok(("", vec![("{{Change}}", 0, 10), ("{{Change}}", 14, 24), ("{{Change}}", 33, 43), ("{{Change}}", 43, 53)])));
 /// ```
-pub fn alternate<I, O, O2, E, F, G>(mut sep: G, mut f: F) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
+pub fn alternate<I, O, O2, E, F, G>(
+    mut sep: G,
+    mut f: F,
+) -> impl FnMut(I) -> IResult<I, Vec<(O, usize, usize)>, E>
 where
     I: Clone + InputLength,
     F: Parser<I, O, E>,
@@ -95,6 +139,7 @@ where
 {
     move |mut i: I| {
         let mut res = Vec::new();
+        let mut junk_count: usize = 0;
 
         loop {
             let len = i.input_len();
@@ -102,7 +147,7 @@ where
                 Err(Err::Error(_)) => return Ok((i, res)),
                 Err(e) => return Err(e),
                 Ok((i1, o)) => {
-                    res.push(o);
+                    res.push((o, junk_count, junk_count + len - i1.input_len()));
                     match sep.parse(i1.clone()) {
                         Err(Err::Error(_)) => return Ok((i1, res)),
                         Err(e) => return Err(e),
@@ -113,6 +158,7 @@ where
                                     ErrorKind::SeparatedList,
                                 )));
                             }
+                            junk_count += len - i2.input_len();
                             //res.push(o);
                             i = i2;
                         }
@@ -155,18 +201,33 @@ mod tests {
     #[test]
     fn test_nom2() {
         assert_eq!(
-            find_blocks("{{AvgFreq}}{{/AvgFreq}}{{Change}}{{/Change}}"),
-            Ok(("", vec![("AvgFreq", "", ""), ("Change", "", "")]))
+            find_blocks("{{Change Weekly}}Weekly change was {{fig}}.{{/Change}}"),
+            Ok((
+                "",
+                vec![Block {
+                    ..Default::default()
+                }]
+            ))
         );
 
         assert_eq!(
-            find_blocks("{{Change Weekly}}Weekly change of {{fig}}.{{/Change}} junk {{Change Daily}}Daily change of {{fig}}.{{/Change}} more junk {{AvgFreq Monthly}} compared to last month where {{fig}} was observed.{{/AvgFreq}} final junk"),
-            Ok((" final junk", vec![("Change", " Weekly", "Weekly change of {{fig}}."), ("Change", " Daily", "Daily change of {{fig}}."), ("AvgFreq", " Monthly", " compared to last month where {{fig}} was observed.")]))
-        );
+            find_blocks("{{Change Weekly}}Weekly change was {{fig}}.{{/Change}} junk {{Change Daily}}Daily change of {{fig}}.{{/Change}} more junk {{AvgFreq Monthly}} compared to last month where {{fig}} was observed.{{/AvgFreq}} final junk"),
+            Ok((" final junk", vec![
+                Block {..Default::default()}, Block { vars: "Daily", template: "Daily change of {{fig}}.", start: 60, end: 111, ..Default::default()}, Block { keyword: "AvgFreq", vars: "Monthly", template: " compared to last month where {{fig}} was observed.", start: 122, end: 204}
+            ])));
 
         assert_eq!(
             find_blocks("junk {{Change}}{{/Change}}"),
-            Ok(("", vec![("Change", "", "")]))
+            Ok((
+                "",
+                vec![Block {
+                    vars: "",
+                    template: "",
+                    start: 5,
+                    end: 26,
+                    ..Default::default()
+                }]
+            ))
         );
     }
 }
